8b0357623f9e6074c7290a589e963b0da587af92
Ke Wu <ke.wu@ibeca.me>
2021-01-15 00:19:13 -0800

    simplifies chpt_5 exec_8

--- src/main/scala/chpt_5/exercise_8.scala
+++ src/main/scala/chpt_5/exercise_8.scala
@@ -4,13 +4,10 @@ object exercise_8 {
   def main(args: Array[String]): Unit = {
     var c1 = new Car("GTI", "VW")
     var c2 = new Car("GTI", "VW", 2019)
-    var c3 = new Car("GTI", "VW", "CoolPlate")
+    var c3 = new Car("GTI", "VW", license = "CoolPlate")
     var c4 = new Car("GTI", "VW", 2019, "CoolPlate")
   }
   class Car(val model: String, val manufacturer: String, val year: Int = -1, var license: String = "") {
-    def this(model: String, manufacturer: String, license: String) {
-      this(model, manufacturer, license = license)
-    }
   }
 
 }
